b482482d78f8ac4d98f1b56789d522b67f1a79af
Thomas Misson <11tmisson@gmail.com>
2022-02-08 10:46:18 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import App from './App.vue'
 import router from './router'
-
+//Test change
 const app = createApp(App);
 app.use(router);
 app.use(ElementPlus);
