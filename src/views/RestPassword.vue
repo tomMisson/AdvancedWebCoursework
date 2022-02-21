5aec198ab511e7d18b8f1fe9cd7e2c40b3ba37f2
wjy4 <W1097304549@gmail.com>
2022-02-21 11:23:32 +0000

    Update RestPassword.vue

--- src/views/RestPassword.vue
+++ src/views/RestPassword.vue
@@ -1,53 +1,57 @@
 <template>
-    <div class="emailcontainer">
-      <h2>Forgotten Password?</h2>
-       <input type = "text" class = "textbox" placeholder="Email" v-model="email"/>
-        <input @click="isShowing ^= true" type = "submit" class = "li" value="Send">
-        <h1 v-show="isShowing"> Email has been sent to you, Please check your emails and verfity. </h1>  
+  <div class="emailcontainer">
+    <h2>Forgotten Password?</h2>
+      <form @submit.prevent="RestPassword">
+       <input v-model="email" type = "text" class = "textbox" placeholder="Email"/>
+        <input type = "submit" class = "li" value="Send">
     <p> Need an account ? </p>
+      </form>
   </div>
 </template>
 
 <script>
 import {ref} from 'vue';
-import { useRoute } from "vue-router"; // <=== 2.
+import { useRoute } from "vue-router";
 import firebase from 'firebase';
 export default {
-setup() {
-    const user = "Emails send";
+  setup() 
+  {
     const email = ref("");
     const route = useRoute(); 
-    const Login = ()=>{
+    const Sendmessage = ("Email has been sent to you, Please check and verfity.");
+
+  const RestPassword = ()=>{
       firebase
         .auth()
         .sendPasswordResetEmail(email.value)
+        .then(function() {
+         window.alert("Email has been sent to you, Please check and verfity. ");
+         Sendmessage;
+        })
         .catch(err =>alert(err.message));
-    },
-    return { route, email, Login, user };
-  },
-   data() {
-    return {
-      isShowing:false,
     }
-  }
+    return { RestPassword,
+            email,
+            route
+            };
+  },
+ 
 }
 </script>
 
 <style>
 h2{
-     margin-top: 50px;
-     font-size: 30px;
+  margin-top: 50px;
+  font-size: 30px;
 }
 .textbox{
-    width: 100%;
-    height:40px;
-    font-size:14pt;
-    
-
+  width: 100%;
+  height:40px;
+  font-size:14pt;
 }
 .li{
-    margin-top: 20px;
-     font-size:14pt;
+  margin-top: 20px;
+  font-size:14pt;
 }
 div.emailcontainer{
   background-color: lightgrey;
@@ -58,3 +62,4 @@ div.emailcontainer{
   height: 500px;
 }
 </style>
+
