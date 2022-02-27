6d673381c8b6ee21069a1602c2714d44eec102e9
tomMisson <11tmisson@gmail.com>
2022-02-27 20:15:32 +0000

    Fix inputs ahead of data visualisation

--- src/views/QueryData.vue
+++ src/views/QueryData.vue
@@ -1,97 +1,64 @@
 <template>
-  <div class="querydata">
-    <div class="container">
-      <div class="row">
-        <div class="col-3">
-          <h4>Cardiomyopathy Type</h4>
-          <br />
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown button
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-3">
-          <h4>Gene mutation</h4>
-          <br />
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown button
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-3">
-          <h4>First dimension</h4>
-          <br />
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown button
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-3">
-          <h4>Second dimension</h4>
-          <br />
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown button
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>Cardiomyopathy Type</h4>
+        <select class="form-select" id="CMtype">
+          <option value="HCM">Hypertrophic</option>
+          <option value="DIL">Dilated</option>
+          <option value="RES">Restrictive</option>
+        </select>
       </div>
-      <div class="row my-5">
-        <div class="graph_place">
-          <h1 class="text-center">Graph Place</h1>
-        </div>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>Gene mutation</h4>
+        <select class="form-select" id="Gene">
+          <option value="HCM">Hypertrophic</option>
+          <option value="DIL">Dilated</option>
+          <option value="RES">Restrictive</option>
+        </select>
       </div>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>First dimension</h4>
+        <select class="form-select" id="fd">
+          <option value="HCM">Hypertrophic</option>
+          <option value="DIL">Dilated</option>
+          <option value="RES">Restrictive</option>
+        </select>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>Second dimension</h4>
+        <select class="form-select" id="sd">
+          <option value="HCM">Hypertrophic</option>
+          <option value="DIL">Dilated</option>
+          <option value="RES">Restrictive</option>
+        </select>
+      </div>
+      <button class="btn btn-success mt-3" id="submit" @click="generateGraph">
+        Generate
+      </button>
+    </div>
+    <div class="row">
+      <h2 class="text-center mt-5">Graph type</h2>
+      <label>
+        <input type="radio" name="graph" value="bar" />
+        Bar
+      </label>
+    </div>
+    <div class="row my-5">
+      <h2 class="text-center mt-2">Graph</h2>
+      <!-- GRAPH HERE -->
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  setup() {
+    function generateGraph() {
+      console.log("Generate graph");
+    }
+
+    return { generateGraph };
+  },
+};
+</script>
