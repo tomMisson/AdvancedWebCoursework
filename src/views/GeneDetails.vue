6c21863a21eaa4f3b39378374596cba999c6ad73
iiEnchant <nawalmaeruf2@gmail.com>
2022-02-28 15:30:04 +0000

    commit before pull master

--- src/views/GeneDetails.vue
+++ src/views/GeneDetails.vue
@@ -1,5 +1,49 @@
+<!-- This page needs work, just delete all the code here, it's just duplicated from the QueryData file/page -->
+
 <template>
-  <div class="genedetails">
-    <h1>Gene details shown here</h1>
+  <h1>Mutation X</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>Gene Type</h4>
+        <select class="form-select" id="CMtype">
+          <option
+            v-for="CMtype in CMtypes"
+            :value="CMtype.shortName"
+            :key="CMtype.shortName"
+          >
+            {{ CMtype.name }}
+          </option>
+        </select>
+      </div>
+
+      <p>Text goes here</p>
+      <p>
+        <!-- GRAPH HERE -->
+      </p>
+    </div>
   </div>
 </template>
+
+<script>
+import { dimensions, mutationTypes, CMtypes } from "../scripts/graphing";
+export default {
+  setup() {
+    function generateGraph() {
+      console.log("Generate graph");
+    }
+
+    return { generateGraph, dimensions, mutationTypes, CMtypes };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+p {
+  text-align: center;
+}
+</style>
