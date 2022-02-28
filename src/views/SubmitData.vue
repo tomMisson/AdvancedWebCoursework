9a41293f8aadcef0981f55554a05147ecee44dc8
wjy4 <W1097304549@gmail.com>
2022-02-28 11:49:52 +0000

    Update SubmitData.vue

--- src/views/SubmitData.vue
+++ src/views/SubmitData.vue
@@ -28,6 +28,15 @@
     <h2>Manual data insert</h2> 
     <form id="manualdata" v-if="!isProcessing"> 
      <h5> (All input must be a number)</h5>
+     <p>Cardiomyopathy Type：
+    <select v-model="scar">
+      <option>Dilated cardiomyopathy</option>
+      <option>Hypertrophic cardiomyopathy</option>
+      <option>Restrictive cardiomyopathy</option>
+      <option>Arrhythmogenic right ventricular dysplasia</option>
+      <option>Unclassified cardiomyopathy</option>
+    </select>
+   </p>
   <p>Left ventricular end diastolic volume (LEDV) :</p>
          <input type="text" v-model="LEDV" @keypress="isNumber($event)"> 
   <p>Right ventricular end diastolic volume (REDV) :</p>
@@ -87,6 +96,17 @@
     <input type="radio" name="myectomy" value="Yes" v-model="myectomy"> Yes
     <input type="radio" name="myectomy" value="No" v-model="myectomy"> No
   </p>
+    <p>HCM gene mutations :  </p>
+    <input type="radio" name="mutations" value="MYH7" v-model="myectomy"> MYH7
+    <input type="radio" name="mutations" value="MYBPC3" v-model="myectomy"> MYBPC3
+    <input type="radio" name="mutations" value="TNNT2" v-model="myectomy"> TNNT2
+    <input type="radio" name="mutations" value="ACTC" v-model="myectomy"> ACTC
+    <input type="radio" name="mutations" value="TPM1" v-model="myectomy"> TPM1
+    <input type="radio" name="mutations" value="TNNCI" v-model="myectomy"> TNNCI
+    <input type="radio" name="mutations" value="TNNI3" v-model="myectomy"> TNNI3
+    <input type="radio" name="mutations" value="MYL2" v-model="myectomy"> MYL2
+    <input type="radio" name="mutations" value="TTN" v-model="myectomy"> TTN
+ <p></p>
    <button class="btn btn-warning" @click="onSubmit"> Submit data </button>
   </form>
 
