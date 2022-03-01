8b4739fd99e48d416d9b38bc9b9492f76a143ece
ismael <ismaelarshad@gmail.com>
2022-03-01 14:11:48 +0000

    displaying and deleting, not able to edit

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" @click="handleClick">
     <template v-if="user">
       <h1>Welcome back {{ user?.displayName }}</h1>
       <br />
@@ -33,6 +33,14 @@ export default {
       return store.getters.userData;
     });
 
+    function handleClick(e) {
+      if (e.target.matches("#delete-btn")) {
+        deleteDocument(e.target.parentElement);
+      } else if (e.target.matches("#edit-btn")) {
+        router.push({ path: "/" });
+      }
+    }
+
     getData();
 
     function getData() {
@@ -49,33 +57,76 @@ export default {
     }
 
     function displayData(querySnapshot) {
+      var count = 0;
       querySnapshot.forEach((doc) => {
+        count++;
         console.log(doc.id, " => ", doc.data());
+        var htmlToSet = "";
         var card = document.getElementById("userData");
-        card.innerHTML =
-          '<div class="card" id="userIdCard"> <div class="card-header" id="' +
+        htmlToSet =
+          '<div class="card" id="' +
+          doc.id +
+          '"> <div class="card-header" id="' +
           '"> <h5 class="mb-0"> <button class="btn" data-toggle="collapse" ' +
-          ' data-target="#collapse-1" aria-expanded="true" aria-controls="collapse-1" ' +
-          '> Document ID: 1232543 </button> </h5> </div> <div id="collapse-1" ' +
-          ' class="collapse show" aria-labelledby="1" data-parent="#userData" > ' +
+          ' data-target="#collapse-' +
+          count +
+          '" aria-expanded="false" aria-controls="collapse-' +
+          count +
+          '" ' +
+          "> Document ID: " +
+          doc.id +
+          ' </button> </h5> </div> <div id="collapse-' +
+          count +
+          '" ' +
+          ' class="collapse" aria-labelledby="' +
+          count +
+          '" data-parent="#userData" > ' +
           ' <div class="card-body"> <table class="table table-sm userData"> <thead> ' +
           ' <tr> <th scope="col">Patient Data</th> <th class="col-md-3" ' +
-          ' scope="col">Value</th> </tr> </thead> <tbody> ' +
-          '<tr> <th scope="row">something</th> <td> '+ doc.data().testing +' </td> ' +
-          ' </tr> <tr> <th scope="row">true/false</th> <td>true</td> </tr> ' +
-          '<tr> <th scope="row">value</th> <td>44.13444</td> </tr> </tbody> ' +
-          ' </table> <div class="d-flex justify-content-center">' +
-          ' <button type="button" class="btn btn-primary btn-block col-p-3 ' +
-          ' action">Edit</button> <button type="button" class="btn btn-danger ' +
-          ' btn-block action">Delete</button> </div> </div> </div> </div> ';
+          ' scope="col">Value</th> </tr> </thead> <tbody> ';
+
+        for (const prop in doc.data()) {
+          var tableRow =
+            '<tr> <th scope="row">' +
+            prop +
+            "</th> <td> " +
+            doc.get(prop) +
+            " </td> </tr>";
+          htmlToSet += tableRow;
+        }
+
+        htmlToSet +=
+          '</tbody></table> <div id="' +
+          doc.id +
+          '" class="d-flex justify-content-center">' +
+          ' <button type="button" id="edit-btn" class="btn btn-primary btn-block col-p-3 ' +
+          ' action">Edit</button> <button type="button" id="delete-btn" class="btn btn-danger ' +
+          " btn-block action " +
+          doc.id +
+          '">Delete</button> </div> </div> </div> </div> ';
+
+        card.innerHTML += htmlToSet;
       });
+    }
+
+    function deleteDocument(docCard) {
+      firestore
+        .collection("patientData")
+        .doc(docCard.id)
+        .get()
+        .then((doc) => {
+          console.log(doc.data());
+          doc.ref.delete();
+          console.log("document deleted");
+          document.getElementById(docCard.id).remove();
+        });
     }
 
     function notLoggedInRedirect() {
       router.push({ path: "/" });
     }
 
-    return { user, notLoggedInRedirect, getData };
+    return { user, notLoggedInRedirect, getData, deleteDocument, handleClick };
   },
 };
 </script>
